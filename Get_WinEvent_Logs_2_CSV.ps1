49840c975253a9b4233eb649f1db6122795ceda4
mrtimd <63519843+mrtimd@users.noreply.github.com>
2025-04-22 12:54:53 -0700

    Add files via upload

--- Get_WinEvent_Logs_2_CSV.ps1
+++ Get_WinEvent_Logs_2_CSV.ps1
@@ -1,3 +1,3 @@
-Get-WinEvent -Path "C:\Path\To\YourLog.evtx" | 
+Get-WinEvent -Path "C:\Windows\System32\winevt\Logs\YourLog.evtx" | 
     Select-Object TimeCreated, Id, LevelDisplayName, Message | 
     Export-Csv -Path "C:\Path\To\Output.csv" -NoTypeInformation
--- Get_WinEvent_Logs_2_JSON.ps1
+++ Get_WinEvent_Logs_2_JSON.ps1
@@ -1,4 +1,4 @@
-Get-WinEvent -Path "C:\Path\To\YourLog.evtx" | 
+Get-WinEvent -Path "C:\Windows\System32\winevt\Logs\YourLog.evtx" | 
     Select-Object TimeCreated, Id, LevelDisplayName, Message | 
     ConvertTo-Json | 
     Out-File -FilePath "C:\Path\To\Output.json"
